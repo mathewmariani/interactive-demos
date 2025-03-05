8d8ac6a4135321732ddc21062972a29748fc3398
mathewmariani <someone.mariani@gmail.com>
2025-03-05 12:23:09 -0500

    Converted BreadthFirstSearch to vue

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ export const SITE_TITLE = 'Interactive Demos';
 export const SITE_DESCRIPTION = '';
 
 export const SYMLINK_SRC = 'build/Release';
-export const SYMLINK_DST = 'public/modules';
+export const SYMLINK_DST = 'src/modules';
 
 export const HomeLink = (): string => '/';
 export const MinesweeperLink = (): string => 'minesweeper';
