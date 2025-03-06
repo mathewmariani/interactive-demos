c9f91b8b22781ce30cb0cef66abd5aca55307c50
mathewmariani <someone.mariani@gmail.com>
2025-03-06 14:00:39 -0500

    Quick cleanup

--- src/integrations/symlink.ts
+++ src/integrations/symlink.ts
@@ -6,7 +6,6 @@ import path from 'path';
 export const symlinkIntegration = () => {
   const symlinks = [
     { source: SYMLINK_SRC, target: SYMLINK_DST },
-    // { source: SYMLINK_SRC, target: "public/modules" },
   ] as const;
 
   let integration: AstroIntegration = {
